956056f6753067aebcf2621db9196002c5e25852
zahid-bracu <zahid.mym@gmail.com>
2021-05-05 22:51:32 +0600

    ok

--- jwt.js
+++ jwt.js
@@ -5,13 +5,13 @@ const jwt =require('jsonwebtoken');
 const createToken= async () =>{
 
     // token generate
-    const token = await jwt.sign({_id:"34258"},'qotyrhepas', {expiresIn:"2 seconds"});
+    const token = await jwt.sign({_id:"34258"},'paokgetolkfredklfgheqwoptgheserk', {expiresIn:"2 seconds"});
     console.log(`Token : ${token}`);
 
 
 
     //verify
-    const userVer= await jwt.verify(token,'qotyrhepas');
+    const userVer= await jwt.verify(token,'paokgetolkfredklfgheqwoptgheserk');
     console.log(userVer);
 }
 
